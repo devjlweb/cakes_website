14878775433655de32060a0f0729c97042dde002
devjlweb <devjlweb@gmail.com>
2022-07-16 05:19:27 +0800

    All set - business pics to attach next

--- main.js
+++ main.js
@@ -47,9 +47,9 @@ class MyFooter extends HTMLElement {
           <h6>Quick links</h6>
           <ul class="list-unstyled">
             <li><a href="index.html">Home</a></li>
-            <li><a href="#">Products</a></li>
+            <li><a href="#pricing">Products</a></li>
             <li><a href="#">Blog</a></li>
-            <li><a href="#">Testimonials</a></li>
+            <li><a href="#testi">Testimonials</a></li>
             <li><a href="contact.html">Contact</a></li>
           </ul>
         </div>
@@ -58,27 +58,27 @@ class MyFooter extends HTMLElement {
           <ul class="list-unstyled">
             <li><a href="#0">Legal information</a></li>
             <li><a href="#0">Job opportunities</a></li>
-            <li><a href="#0">Frequently Asked Questions</a></li>
+            <li><a href="#faqs">Frequently Asked Questions</a></li>
           </ul>
         </div>
         <div class="col-12 col-sm-6 col-md-3">
           <h6>Follow us</h6>
           <ul class="list-unstyled">
-            <li><a href="#0"><i class="fab fa-fw fa-facebook"></i> Facebook</a></li>
-            <li><a href="#0"><i class="fab fa-fw fa-instagram"></i> Instagram</a></li>
+            <li><a href="https://www.facebook.com/ryleskitchen"><i class="fab fa-fw fa-facebook"></i> Facebook</a></li>
+            <li><a href="https://www.instagram.com/ryles_kitchen/"><i class="fab fa-fw fa-instagram"></i> Instagram</a></li>
             <li><a href="#0"><i class="fab fa-fw fa-twitter"></i> Twitter</a></li>
             <li><a href="#0"><i class="fab fa-fw fa-youtube"></i> YouTube</a></li>
             <li><a href="#0"><i class="fab fa-fw fa-linkedin"></i> LinkedIn</a></li>
           </ul>
         </div>
         <div class="col-12 col-sm-6 col-md-3">
-          <h6>Our location</h6>
+          <h6>#4 Fasa Ave. Martinar, Nadi</h6>
           <address>
-            <strong>Company Name</strong><br>
-            Steet Name<br>
-            ZIP code and city<br>
-            <i class="fas fa-fw fa-phone"></i> <a href="tel:+1234567890">(123) 456-7890</a><br>
-            <i class="fas fa-fw fa-envelope"></i> <a href="mailto:info@domain.com">info@domain.com</a>
+            <strong>Ryle's Kitchen</strong><br>
+            Fasa Ave.<br>
+            Martinar, Nadi<br>
+            <i class="fas fa-fw fa-phone"></i> <a href="tel:+6799033297">(679) 903-3297</a><br>
+            <i class="fas fa-fw fa-envelope disabled"></i> <a href="mailto:info@domain.com">info@domain.com</a>
           </address>
         </div>
       </div>
